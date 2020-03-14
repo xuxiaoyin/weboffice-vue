06596262b3c69391cc8b6bd6956040d17d5cbeeb
xuxiaoyin <279238850@qq.com>
2020-03-14 23:54:28 +0800

    日历

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "office" */ '../views/Office.vue')
   },
+  {
+    path: '/fullcalendar',
+    name: 'Fullcalendar',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "fullcalendar" */ '../views/Fullcalendar.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Fullcalendar.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class='demo-app'>
+    <div class='demo-app-top'>
+      <button @click="toggleWeekends">toggle weekends</button>
+      <button @click="gotoPast">go to a date in the past</button>
+      (also, click a date/time to add an event)
+    </div>
+    <FullCalendar
+      class='demo-app-calendar'
+      ref="fullCalendar"
+      defaultView="dayGridMonth"
+      :header="{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      }"
+      :plugins="calendarPlugins"
+      :weekends="calendarWeekends"
+      :events="calendarEvents"
+      locale="zh-cn"
+      firstDay="1"
+      weekNumberCalculation="ISO" 
+      :buttonText="buttonText"
+      @dateClick="handleDateClick"
+      :droppable="true"
+      :editable="true"
+      :selectable="true"
+      @select="daySelect"
+      />
+  </div>
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list';
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data: function() {
+    return {
+      calendarPlugins: [ // plugins must be defined in the JS
+        dayGridPlugin,
+        timeGridPlugin,
+        interactionPlugin, // needed for dateClick
+        listPlugin
+      ],
+      calendarWeekends: true,
+      calendarEvents: [ // initial event data
+        { title: 'Event Now', start: new Date() }
+      ],
+      buttonText: {
+        prev: '上月', // ‹
+        next: '下月', // ›
+        prevYear: '去年', // «
+        nextYear: '明年', // »
+        today: '今天',
+        month: '月',
+        week: '周',
+        day: '天',
+        list: '列表'
+      }
+    }
+  },
+  methods: {
+    toggleWeekends() {
+      this.calendarWeekends = !this.calendarWeekends // update a property
+    },
+    gotoPast() {
+      let calendarApi = this.$refs.fullCalendar.getApi() // from the ref="..."
+      calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+    },
+    handleDateClick(arg) {
+      if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+        this.calendarEvents.push({ // add new event data
+          title: 'New Event',
+          start: arg.date,
+          allDay: arg.allDay
+        })
+      }
+    },
+    daySelect(arg) {
+      console.log(arg)
+    }
+  }
+}
+</script>
+
+<style>
+@import '~@fullcalendar/core/main.css';
+@import '~@fullcalendar/daygrid/main.css';
+@import '~@fullcalendar/timegrid/main.css';
+.demo-app {
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+.demo-app-top {
+  margin: 0 0 3em;
+}
+.demo-app-calendar {
+  margin: 0 auto;
+  max-width: 900px;
+}
+</style>
